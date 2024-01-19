1335d778b705a2acee7ea406ba5ce84265935d3e
Stas <garrixdit@gmail.com>
2024-01-19 20:41:23 +0200

    save

--- src/app/(globalheader)/account/page.tsx
+++ src/app/(globalheader)/account/page.tsx
@@ -6,53 +6,7 @@ import { OrderCard } from '@/components/custom/cards/ordercard/ordercard';
 export default function ReturnsPage() {
 	return (
 		<Fragment>
-			<div className="grow border-2 border-dark/80 p-[1rem] flex flex-col gap-[1rem]">
-				<OrderCard
-					src={'https://content2.rozetka.com.ua/goods/images/big/218279819.jpg'}
-					title={'Globe'}
-					desc={'Classic - 40'}
-					size={'xl'}
-					quantity={2}
-					total={'750'}
-					shipping={'Italy'}
-				/>
-				<OrderCard
-					src={'https://content2.rozetka.com.ua/goods/images/big/218279819.jpg'}
-					title={'Globe'}
-					desc={'Classic - 40'}
-					size={'xl'}
-					quantity={2}
-					total={'750'}
-					shipping={'Italy'}
-				/>
-				<OrderCard
-					src={'https://content2.rozetka.com.ua/goods/images/big/218279819.jpg'}
-					title={'Globe'}
-					desc={'Classic - 40'}
-					size={'xl'}
-					quantity={2}
-					total={'750'}
-					shipping={'Italy'}
-				/>
-				<OrderCard
-					src={'https://content2.rozetka.com.ua/goods/images/big/218279819.jpg'}
-					title={'Globe'}
-					desc={'Classic - 40'}
-					size={'xl'}
-					quantity={2}
-					total={'750'}
-					shipping={'Italy'}
-				/>
-				<OrderCard
-					src={'https://content2.rozetka.com.ua/goods/images/big/218279819.jpg'}
-					title={'Globe'}
-					desc={'Classic - 40'}
-					size={'xl'}
-					quantity={2}
-					total={'750'}
-					shipping={'Italy'}
-				/>
-			</div>
+			<div className="grow border-2 border-dark/80 p-[1rem] flex flex-col gap-[1rem]"></div>
 			<div className="w-[30%] h-min max-h-[60vh] bg-gold sticky top-[5vh] font-spacemono flex flex-col flex-wrap gap-[1rem] p-[1.5rem] rounded-sm">
 				<h2 className="text-2xl font-black">Order Details</h2>
 				<div className="flex justify-between gap-[1rem] font-semibold text-[16px]">
